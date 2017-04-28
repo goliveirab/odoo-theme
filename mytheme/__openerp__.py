041500421eb57bcc5b9e14a70dce10b4c12457c0
Germana Oliveira <germana@vauxoo.com>
2017-04-28 19:23:02 +0000

    [ADD] odoo-theme: add config fi

---
+++ mytheme/__openerp__.py
@@ -0,0 +1,12 @@
+{
+  'name':'Tutorial theme',
+  'description': 'A first theme for Odoo',
+  'version':'1.0',
+  'author':'goliveirab',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+  'application': True,
+}
